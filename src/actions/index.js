9000a0fbf3efc196c86bd06d02e2106e13188f16
hcvazquez <hernan.c.vazquez@gmail.com>
2017-10-26 10:37:47 -0300

    Initial structure

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const FETCH_POST = 'fetch_posts';
+
+const ROOT_URL = '	http://reduxblog.herokuapp.com/api'
+const API_KEY = '?key=KEYBANA123'
+
+export function fetchPosts(){
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+    return{
+        type: FETCH_POST,
+        payload: request
+    };
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter,Route} from 'react-router-dom';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
+import PostsIndex from './components/posts_index'
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
